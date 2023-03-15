08162507b935338d32a3d28b535ffb8e4969a373
lbc00 <lbc00@nate.com>
2023-03-15 20:27:33 +0900

    HelloController 추가

--- src/main/java/com/example/githubpullrequest/controller/HelloController.java
+++ src/main/java/com/example/githubpullrequest/controller/HelloController.java
@@ -1,4 +1,10 @@
 package com.example.githubpullrequest.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+    public String hello() {
+        return "hello";
+    }
 }
