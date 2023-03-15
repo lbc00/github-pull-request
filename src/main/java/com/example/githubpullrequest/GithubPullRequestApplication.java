b4e124ed3d005c151eb2ce3b231d073ad1ff8c7a
lbc00 <lbc00@nate.com>
2023-03-15 19:55:41 +0900

    Initial commit

---
+++ src/main/java/com/example/githubpullrequest/GithubPullRequestApplication.java
@@ -0,0 +1,13 @@
+package com.example.githubpullrequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubPullRequestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubPullRequestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/githubpullrequest/GithubPullRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.githubpullrequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubPullRequestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
