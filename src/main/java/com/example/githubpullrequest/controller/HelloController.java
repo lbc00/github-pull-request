f3102844a7b8dbed3986fef2496db07a3ebaa45f
lbc00 <lbc00@nate.com>
2023-03-15 22:15:33 +0900

    HelloController hello api return type 수정

--- src/main/java/com/example/githubpullrequest/controller/HelloController.java
+++ src/main/java/com/example/githubpullrequest/controller/HelloController.java
@@ -1,12 +1,13 @@
 package com.example.githubpullrequest.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
-        return "hello";
+    public ResponseEntity<?> hello() {
+        return ResponseEntity.ofNullable("hello");
     }
 }
